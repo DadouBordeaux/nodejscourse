32f713dab551f2aa7516b6f32fb30dba55a0219b
Dadou <jorisdamien@gmail.com>
2018-04-23 22:59:07 +0200

    log requested path

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+// primary file for the API
+
+// Dependencies
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer(function(req, res) {
+  // get url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  // get the path from url
+  const path = parsedUrl.pathname;
+  const trimedPath = path.replace(/^\/|\/$/g, '');
+  // send a response
+  res.end('hello world\n');
+  // log the request path
+  console.log('request recieved on : ', trimedPath);
+});
+
+server.listen(3000, function() {
+  console.log('The server is listening on port 3000 now');
+});
