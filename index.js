bb25f9ad76a308964e2b96d234585b10c370268c
Dadou <jorisdamien@gmail.com>
2018-05-01 17:12:36 +0200

    use of handler

--- index.js
+++ index.js
@@ -3,19 +3,81 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer(function(req, res) {
   // get url and parse it
   const parsedUrl = url.parse(req.url, true);
   // get the path from url
   const path = parsedUrl.pathname;
-  const trimedPath = path.replace(/^\/|\/$/g, '');
-  // send a response
-  res.end('hello world\n');
-  // log the request path
-  console.log('request recieved on : ', trimedPath);
+  const trimmedPath = path.replace(/^\/|\/$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // get the HTTP method
+  const method = req.method.toLowerCase();
+
+  const headers = req.headers;
+
+  //get the payload if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function() {
+    buffer += decoder.end();
+
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined'
+      ? router[trimmedPath]
+      : handlers.notFound
+
+    const data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'headers': headers,
+      'method': method,
+      'headers': headers
+    };
+    // send a response
+    chosenHandler(data, function(statusCode, payload) {
+      //init default status statusCode
+      statusCode = typeof(statusCode) === 'number'
+        ? statusCode
+        : 200;
+      // init default payload
+      payload = typeof(payload) === 'object'
+        ? payload
+        : {};
+
+      const payloadString = JSON.stringify(payload);
+
+      res.writeHead(statusCode);
+      res.end(payloadString);
+    });
+
+  });
+
 });
 
 server.listen(3000, function() {
   console.log('The server is listening on port 3000 now');
 });
+
+// define handlers
+const handlers = {};
+
+handlers.sample = function(data, callback) {
+  callback(406, {'name': 'sample handler'});
+};
+
+handlers.notFound = function(data, callback) {
+  callback(404);
+};
+// define router
+const router = {
+  'sample': handlers.sample
+};
