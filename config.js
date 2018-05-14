74eca067c2b47e10091ec745fbeff70b2d35fc46
Dadou <jorisdamien@gmail.com>
2018-05-14 22:32:59 +0200

    add https

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 const environements = {};
 
 environements.stagin = {
-  port: 3000,
+  httpPort: 3000,
+  httpsPort: 3001,
   envName: 'stagin'
 }
 
 environements.production = {
-  port: 5000,
+  httpPort: 5000,
+  httpsPort: 5001,
   envName: 'production'
 }
 
--- index.js
+++ index.js
@@ -2,23 +2,44 @@
 
 // Dependencies
 const http = require('http');
+const https = require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config')
+const fs = require('fs');
 
-const server = http.createServer(function(req, res) {
+
+const httpsServerOptions = {
+  key: fs.readFileSync('./https/key.pem'),
+  cert: fs.readFileSync('./https/certificate.pem')
+};
+
+const httpsServer = http.createServer(httpsServerOptions, function(req, res) {
+  unifiedServer(req, res);
+});
+
+httpsServer.listen(config.httpsPort, function() {
+  console.log('The server is listening on port ' + config.httpsPort);
+});
+
+const httpServer = http.createServer(function(req, res) {
+  unifiedServer(req, res);
+});
+
+httpServer.listen(config.httpPort, function() {
+  console.log('The server is listening on port ' + config.httpPort);
+});
+
+const unifiedServer = ((req, res) => {
   // get url and parse it
   const parsedUrl = url.parse(req.url, true);
   // get the path from url
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/|\/$/g, '');
-
   // Get the query string as an object
   const queryStringObject = parsedUrl.query;
-
   // get the HTTP method
   const method = req.method.toLowerCase();
-
   const headers = req.headers;
 
   //get the payload if any
@@ -33,8 +54,8 @@ const server = http.createServer(function(req, res) {
     buffer += decoder.end();
 
     const chosenHandler = typeof(router[trimmedPath]) !== 'undefined'
-      ? router[trimmedPath]
-      : handlers.notFound
+    ? router[trimmedPath]
+    : handlers.notFound
 
     const data = {
       'trimmedPath': trimmedPath,
@@ -47,26 +68,21 @@ const server = http.createServer(function(req, res) {
     chosenHandler(data, function(statusCode, payload) {
       //init default status statusCode
       statusCode = typeof(statusCode) === 'number'
-        ? statusCode
-        : 200;
+      ? statusCode
+      : 200;
       // init default payload
       payload = typeof(payload) === 'object'
-        ? payload
-        : {};
+      ? payload
+      : {};
 
       const payloadString = JSON.stringify(payload);
 
-res.setHeader('content-type', 'application/json');
+      res.setHeader('content-type', 'application/json');
       res.writeHead(statusCode);
       res.end(payloadString);
     });
 
   });
-
-});
-
-server.listen(config.port, function() {
-  console.log('The server is listening on port '+config.port+' on '+config.envName);
 });
 
 // define handlers
